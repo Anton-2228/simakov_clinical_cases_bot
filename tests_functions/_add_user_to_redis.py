740979ce9059c12e5057730c307d1f3b9e19abda
anton <anton@zinchenko.pro>
2025-08-10 01:42:49 +0300

    Added testing functions to work with redis DB

---
+++ tests_functions/_add_user_to_redis.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from db.redis import RedisStorage
+from db.service.redis_services import RedisServices
+from enums import USER_TYPE
+from models import User
+
+
+async def main(db: RedisServices, telegram_id: int, full_name: str, role: USER_TYPE):
+    user = User(telegram_id=telegram_id,
+                full_name=full_name,
+                user_type=role)
+    await db.user.save_user(user)
+
+if __name__ == "__main__":
+    REDIS = RedisStorage(host="127.0.0.1", port=6379)
+    DB = RedisServices(redis_client=REDIS)
+    asyncio.run(main(db=DB,
+                     telegram_id=173202775,
+                     full_name="Зинченко Антон Андреевич",
+                     role=USER_TYPE.ADMIN))
