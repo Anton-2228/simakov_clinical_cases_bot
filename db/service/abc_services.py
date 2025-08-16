52b18264d017fa3ceb814399daed08736c908194
anton <anton@zinchenko.pro>
2025-08-16 22:05:55 +0300

    Renamed RedisServices to Services

---
+++ db/service/abc_services.py
@@ -0,0 +1,25 @@
+from abc import abstractmethod, ABC
+
+from db.service.survey.async_survey_service import AsyncSurveyService
+from db.service.survey.survey_service import SurveyService
+from db.service.survey_steps.async_survey_steps_service import AsyncSurveyStepService
+from db.service.survey_steps.survey_steps_service import SurveyStepService
+from db.service.user.async_user_service import AsyncUserService
+from db.service.user.user_service import UserService
+
+
+class ABCServices(ABC):
+    @property
+    @abstractmethod
+    def user(self) -> UserService | AsyncUserService:
+        pass
+
+    @property
+    @abstractmethod
+    def survey_step(self) -> SurveyStepService | AsyncSurveyStepService:
+        pass
+
+    @property
+    @abstractmethod
+    def survey(self) -> SurveyService | AsyncSurveyService:
+        pass
